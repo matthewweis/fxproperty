4d70da03e44dd66f77df91e2548a91ee3b9f7d59
Matthew Weis <matthewweis@ksu.edu>
2023-01-05 19:24:53 -0600

    clean module-info

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -1,9 +1,3 @@
 module org.fxproperty.api {
-//open module org.fxproperty.api {
-//    exports org.fxproperty.api to org.fxproperty.javac;
     exports org.fxproperty.api;
-//    exports org.fxproperty.api;
-//    requires transitive jdk.compiler;
-//    uses javax.annotation.processing.Processor;
-//    provides javax.annotation.processing.Processor with FxPropertyProcessor;
 }
